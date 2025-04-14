95d10efbbecb1980d5e6256c8338d7908cb8cef9
Gaspar Mutti <gasparrobortellamutti@gmail.com>
2025-04-14 11:46:07 -0300

    feat(constants.js): add rows,columns,empty value and original pieces

---
+++ src/domain/constants.js
@@ -0,0 +1,67 @@
+export const ROWS = 20;
+export const COLUMNS = 10;
+export const EMPTY_VALUE = 0;
+export const PIECES = [
+  {
+    id: 1,
+    name: "Smashboy",
+    color: "yellow",
+    matrix: [
+      [1, 1],
+      [1, 1],
+    ],
+  },
+  {
+    id: 2,
+    name: "Hero",
+    color: "cyan",
+    matrix: [[2], [2], [2], [2]],
+  },
+  {
+    id: 3,
+    name: "Teewee",
+    color: "purple",
+    matrix: [
+      [0, 3, 0],
+      [3, 3, 3],
+    ],
+  },
+  {
+    id: 4,
+    name: "Orange Ricky",
+    color: "orange",
+    matrix: [
+      [4, 0],
+      [4, 0],
+      [4, 4],
+    ],
+  },
+  {
+    id: 5,
+    name: "Blue Ricky",
+    color: "blue",
+    matrix: [
+      [0, 5],
+      [0, 5],
+      [5, 5],
+    ],
+  },
+  {
+    id: 6,
+    name: "Rhode Island Z",
+    color: "green",
+    matrix: [
+      [0, 6, 6],
+      [6, 6, 0],
+    ],
+  },
+  {
+    id: 7,
+    name: "Cleveland Z",
+    color: "red",
+    matrix: [
+      [7, 7, 0],
+      [0, 7, 7],
+    ],
+  },
+];
