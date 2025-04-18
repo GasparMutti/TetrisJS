a64cd58f1fb6101d8b284542f9ff274857afce47
Gaspar Mutti <gasparrobortellamutti@gmail.com>
2025-04-18 00:08:36 -0300

    chore(events): add constant events

---
+++ src/tetris/domain/events/events.js
@@ -0,0 +1,12 @@
+export const EVENTS = {
+  PIECE_SPAWNED: "piece:spawned",
+  PIECE_MOVED: "piece:moved",
+  PIECE_MERGED: "piece:merged",
+  PIECE_COLLISION: "piece:collision",
+  TETRIS_LOST: "tetris:lost",
+  BOARD_COMPLETED_ROWS: "board:completedRows",
+  BOARD_DELETED_ROWS: "board:clearedRows",
+  GAME_START: "game:start",
+  GAME_PAUSE: "game:pause",
+  GAME_END: "game:end",
+};
