82595ab05292b6faa92d8e08eae32cade71c4fd6
Gaspar Mutti <gasparrobortellamutti@gmail.com>
2025-04-24 10:15:55 -0300

    feat(KeyboardController.js): Keyboard controller with default controls

--- src/tetris/infrastructure/KeyboardController.js
+++ src/tetris/infrastructure/KeyboardController.js
@@ -1,7 +1,8 @@
 import {EVENTS} from "../domain/events/events";
 
 export class KeyboardController {
-  constructor(eventBus) {
+  constructor({eventsMap, eventBus}) {
+    this.eventsMap = eventsMap ?? this.getDefaultEventsMap();
     this.eventBus = eventBus;
     this.registerEvents();
   }
@@ -16,13 +17,16 @@ export class KeyboardController {
   }
 
   getEventByKey(key) {
-    const eventsMap = {
+    return this.eventsMap[key] ?? null;
+  }
+
+  getDefaultEventsMap() {
+    return {
       arrowup: EVENTS.INPUT_ROTATE,
       arrowright: EVENTS.INPUT_RIGHT,
       arrowdown: EVENTS.INPUT_DOWN,
       arrowleft: EVENTS.INPUT_LEFT,
       p: EVENTS.INPUT_PAUSE,
     };
-    return eventsMap[key] ?? null;
   }
 }
